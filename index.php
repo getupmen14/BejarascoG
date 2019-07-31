2be8d240e42a233412a74779e25077d45f601d4f
getupmen14 <53499681+getupmen14@users.noreply.github.com>
2019-07-31 09:33:20 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,13 @@
 <!DOCTYPE html>
 <html>
   <head>
-    <title>HELLO</title></head>
+    <title>Page Title</title>
+  </head>
   <body>
-    <p>WORLD</p>
+    <h1>Google Chrome<h1>
+      <p>Google Chrome is a cross-platform web browser developed by Google. It was first released in 2008 for Microsoft Windows, and was later ported to Linux, macOS, iOS, and Android. The browser is also the main component of Chrome OS, where it serves as the platform for web apps.</p>
+<details>
+  <summary>Source:</summary>
+  <p>https://www.w3schools.com/html/</p>
+      </details>
+      </body>
