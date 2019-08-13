ef93e6631fa4b33eca33b493f1db6779ceda6539
getupmen14 <53499681+getupmen14@users.noreply.github.com>
2019-08-13 20:32:58 +0800

    Update photo1.php

--- photo1.php
+++ photo1.php
@@ -6,7 +6,7 @@
 	<br>
 	<img src="luffy1.jpg" width="200" height="300">
 </center>
-<a href="Assignment 05.php">Back to Home</a>
+<a href="bejarascog.php">Back to Home</a>
 <div align="right">
 <a href="photo2.php">Next Photo</a>
 </div>
