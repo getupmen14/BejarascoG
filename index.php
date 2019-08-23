a68742e051402726a852d174d3d045a1001db21f
getupmen14 <53499681+getupmen14@users.noreply.github.com>
2019-08-23 21:47:43 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,12 +1,27 @@
 <!DOCTYPE html>
 <html>
-<head><title>Assignmet 05</title></head>
-<body><center>
-	<h1>BejarascoG Photo Gallery</h1>
-	<h2>List of Photos</h2>
-	<a href="photo1.php">Photo 1</a>
-	<br>
-	<a href="photo2.php">Photo 2</a>
-	<br>
-	<a href="photo3.php">Photo 3</a>
+<head><title>Login, Register and Photo Gallery</title></head>
+<body background="background1.jpg"><center>
+	
+
+	<h1>Login In To Your Account</h1>
+	<div class="container">
+		<label for="uname"><b>Username</b></label>
+		<input type="text" placeholder="Enter Username" name="uname" required>
+		</br>
+		</br>
+		<label for="psw"><b>Password</b></label>
+		<input type="password" placeholder="Enter Password" name="psw" required>
+		</br>
+		<a href="home.html"><button type="submit">Sign in</button></a>
+		</br>
+		</br>
+		</br>
+		<a href="resetpassword.html">Reset Password</a>
+		</br>
+		<a href="register.html">Sign Up</a>
+	</div>
+	
+</center>
 </body>
+</html>
