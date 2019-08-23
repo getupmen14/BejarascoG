b2c02aa61df81ebd8cd073a1f5759e50f9c8c648
getupmen14 <53499681+getupmen14@users.noreply.github.com>
2019-08-23 21:50:31 +0800

    Create welcome.php

---
+++ welcome.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head><title>Login, Register and Photo Gallery</title></head>
+<body background="background1.jpg"><center>
+
+	<h1>Welcome to my Photo Gallery</h1>
+
+	<a href="home.php">Proceed to Photo Gallery</a>
+</center>
+</body>
+</html>
